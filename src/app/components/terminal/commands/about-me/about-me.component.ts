5f562a3e4b025c22b1c87a7d9e667cc31ee06edc
Medovyha <maxim.donchyk@gmail.com>
2024-01-18 02:28:12 +0100

    add design + add about me

---
+++ src/app/components/terminal/commands/about-me/about-me.component.ts
@@ -0,0 +1,27 @@
+import {
+    Component,
+    ElementRef,
+    Renderer2,
+    ViewContainerRef
+} from '@angular/core';
+import * as jsonData from "../../../../../assets/settings.json";
+
+@Component({
+    selector: 'app-about-me',
+    standalone: true,
+    imports: [],
+    templateUrl: './about-me.component.html',
+    styleUrl: './about-me.component.scss'
+})
+export class AboutMeComponent {
+    constructor() {
+    }
+
+    settings: any = (jsonData as any).default;
+
+    addDivToInputGroup(viewRef: ViewContainerRef, renderer: Renderer2, el: ElementRef): void {
+        const compRef = viewRef.createComponent(AboutMeComponent)
+        renderer.appendChild(el.nativeElement.querySelector(this.settings.design.parent), compRef.location.nativeElement);
+
+    }
+}
