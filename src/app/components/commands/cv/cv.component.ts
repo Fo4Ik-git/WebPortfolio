67fe1d9af8d22bab2aa6fbf467ecc64acfacad5d
Vlad Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2024-01-29 12:03:08 +0100

    add cv view

--- src/app/components/commands/cv/cv.component.ts
+++ src/app/components/commands/cv/cv.component.ts
@@ -2,32 +2,26 @@ import {Component, ElementRef, Renderer2, ViewContainerRef} from '@angular/core'
 import * as settings from "../../../config/settings.json";
 import {SharedDataService} from "../../../services/shared-data.service";
 import {NgClass} from "@angular/common";
+import {ImageModule} from "primeng/image";
 
 @Component({
-  selector: 'app-cv',
-  standalone: true,
-  imports: [
-    NgClass
-  ],
-  templateUrl: './cv.component.html',
-  styleUrl: './cv.component.scss'
+    selector: 'app-cv',
+    standalone: true,
+    imports: [
+        NgClass,
+        ImageModule
+    ],
+    templateUrl: './cv.component.html',
+    styleUrl: './cv.component.scss'
 })
 export class CvComponent {
-  constructor(public sharedData: SharedDataService) {
-  }
-
-  settings: any = (settings as any).default;
-  isFullScreen: boolean = false;
-
-  addDivToInputGroup(viewRef: ViewContainerRef, renderer: Renderer2, el: ElementRef): void {
-    const compRef = viewRef.createComponent(CvComponent)
-    renderer.appendChild(el.nativeElement.querySelector(this.settings.design.parent), compRef.location.nativeElement);
-  }
-
-  toggleFullScreen() {
-    if (window.innerHeight < 1100 && window.innerWidth < 830) {
-      return;
+    constructor(public sharedData: SharedDataService) {
     }
-    this.isFullScreen = !this.isFullScreen;
-  }
+
+    settings: any = (settings as any).default;
+
+    addDivToInputGroup(viewRef: ViewContainerRef, renderer: Renderer2, el: ElementRef): void {
+        const compRef = viewRef.createComponent(CvComponent)
+        renderer.appendChild(el.nativeElement.querySelector(this.settings.design.parent), compRef.location.nativeElement);
+    }
 }
